6a749a1c3d0008f07efbf6e32988085cc601b6f9
shrutsureja <shrutsureja.code@gmail.com>
2024-01-06 00:34:26 +0530

    copy button frontend with keyword only frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import PasswordDisplay from "./components/PasswordDisplay"
+import PasswordGenerator from "./components/PasswordGenerator"
+
 
 function App() {
 
   return (
     <>
-      <div className="bg-slate-500 text-white">
-        <p>~ShrutSureja</p>
+      <div className="bg-gray-200 h-screen w-screen flex justify-center items-center">
+        <div className="bg-white rounded-md max-w-md w-full shadow-md p-8">
+          <h1 className="text-center font-mono font-[600] text-xl mb-8">Custom Password Generator</h1>
+          <PasswordDisplay/>
+          <div className="mb-4"></div>
+          <PasswordGenerator/>
+        </div>
       </div>
     </>
   )
