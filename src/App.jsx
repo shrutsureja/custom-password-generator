14a149bbdb793806a15f8b4b9415c65b10eab74c
shrutsureja <shrutsureja.code@gmail.com>
2024-01-07 10:45:21 +0530

    added generate password logic but error in display

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-
 import PasswordDisplay from "./components/PasswordDisplay"
 import PasswordGenerator from "./components/PasswordGenerator"
-// import { useState } from "react"
+import { useState } from "react"
+// import { useEffect } from "react"
 import './App.css'
 
 function App() {
-  // const [generatedPassword, setGeneratedPassword] = useState('');
+  const [generatedPassword, setGeneratedPassword] = useState('');
+  // setGeneratedPassword("shrut");
+  // useEffect(()=>{
+  //   setGeneratedPassword("Shrasdat");
+  //   alert("in the useEffect: " + generatedPassword.length);
+  //   console.log("Generated Password: "+ generatedPassword)
+  // },[generatedPassword]);
   return (
     <>
-    shrutsureja
       <div className="bg-gray-200 h-screen w-screen flex justify-center items-center">
         <div className="bg-white rounded-md max-w-md w-full shadow-md p-8">
           <h1 className="text-center font-mono font-[600] text-xl mb-8">Custom Password Generator</h1>
-            <PasswordDisplay/>
-          <div className="mb-4"></div>
-          <PasswordGenerator/>
+            <p>{generatedPassword}</p>
+            <PasswordDisplay passsword={generatedPassword}/>
+            <div className="mb-4"></div>
+            <PasswordGenerator setGeneratedPassword={setGeneratedPassword}/>
         </div>
       </div>
     </>
--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -1,27 +1,58 @@
+import React from "react";
+import { useState } from "react";
 
-function PasswordGenerator() {
+function PasswordGenerator({setGeneratedPassword}) {
+    const [passwordLength, setPasswordLength ] = useState(12);
+    const [includeLowerCase, setLowerCase] = useState(true);
+    const [includeUpperCase, setUpperCase] = useState(true);
+    const [includeCharacter, setCharacter] = useState(false);
+    const [includeNumbers, setNumbers] = useState(false);
+
+    function generateNewPassword(){
+        const characters = {
+            uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            lowercase: "abcdefghijklmnopqrstuvwxyz",
+            numbers: "0123456789",
+            specialSymbols: "!@#$%^&*()_+[]{}|;:,.<>?",
+        };
+        let set = '';
+        if(includeLowerCase)set+=characters.lowercase;
+        if(includeCharacter)set+=characters.specialSymbols;
+        if(includeNumbers)set+=characters.numbers;
+        if(includeUpperCase)set+=characters.uppercase;
+        
+        let newPassword ='';
+        for(let i = 0; i < passwordLength; i++){
+            newPassword += set.charAt(Math.floor(Math.random()*set.length));
+        }
+        console.log(newPassword);
+        setGeneratedPassword(newPassword);
+    }
+    
     
     return(
         <>
             <div className="mb-4">
                 <label className="block text-sm font-medium text-gray-600">Password Length:</label>
-                <input className="border-2 border-gray-300 w-full rounded-md p-2" type="number" min={1} max={24}/>
+                <input className="border-2 border-gray-300 w-full rounded-md p-2" type="number" min={1} max={24} value={passwordLength} onChange={(event)=>setPasswordLength(event.target.value)}/>
             </div>
             <label className="text-sm font-medium text-gray-600">Include:</label>
             <div className="mb-1">
-                <input className="mr-2 border-2 h-full w-auto" type="checkbox" id="lowerCase"/>
+                <input className="mr-2 border-2 h-full w-auto" type="checkbox" id="lowerCase" checked={includeLowerCase} onChange={()=>{setLowerCase(!includeLowerCase)}}/>
+                {/* <input className="mr-2 border-2 h-full w-auto" type="checkbox" id="lowerCase" checked={includeLowerCase} onChange={setLowerCase(!includeLowerCase)}/> */}
+                {/* DOUBT: the Above line which is commented is re-rendering continously why !!*/}
                 <label className="text-md font-normal">LowerCase alphabets</label>
             </div>
             <div className="mb-1">
-                <input className="mr-2 border-2 h-full w-auto" type="checkbox" id="upperCase"/>
+                <input className="mr-2 border-2 h-full w-auto" type="checkbox" id="upperCase" checked={includeUpperCase} onChange={()=>{setUpperCase(!includeUpperCase)}}/>
                 <label className="text-md font-normal">UpperCase alphabets</label>
             </div>
             <div className="mb-1">
-                <input className="mr-2 border-2 h-full w-auto" type="checkbox" id="characters"/>
+                <input className="mr-2 border-2 h-full w-auto" type="checkbox" id="characters" checked={includeCharacter} onChange={()=>{setCharacter(!includeCharacter)}}/>
                 <label className="text-md font-normal">Special characters</label>
             </div>
             <div className="mb-3">
-                <input className="mr-2 border-2 h-full w-auto" type="checkbox" id="numbers"/>
+                <input className="mr-2 border-2 h-full w-auto" type="checkbox" id="numbers" checked={includeNumbers} onChange={()=>{setNumbers(!includeNumbers)}}/>
                 <label className="text-md font-normal">Numbers</label>
             </div>
             <div className="mb-4">
@@ -29,7 +60,7 @@ function PasswordGenerator() {
                 <input className="border-2 border-gray-300 w-full rounded-md p-2" type="text"/>
             </div>
             <div className="flex justify-center">
-                <button className="bg-blue-600 text-white rounded-md p-3">Generate Password</button>
+                <button className="bg-blue-600 text-white rounded-md p-3" onClick={generateNewPassword}>Generate Password</button>
             </div>
         </>
     );
