a06690afeae20430880a1273195ccc91058c4063
shrutsureja <shrutsureja.code@gmail.com>
2024-01-07 11:21:34 +0530

    password display error solved

--- src/components/PasswordDisplay.jsx
+++ src/components/PasswordDisplay.jsx
@@ -1,14 +1,11 @@
-import React from "react"
 import copyImg from "../assets/copy.png"
 
 const PasswordDisplay = ({password}) => {
-    console.log(password);
     return(
         <div>
             <label className="text-sm font-medium text-gray-600">Generated Password:</label>
             <div className="border-2 border-gray-300 rounded-md flex justify-between">
-                <p className={`m-3 ${!password? "text-gray-400":""}`} >{password || "Your password will be here"}</p>
-                <p className={`m-3`} >{password}</p>
+                <input value={password || "Your password will be here"} disabled className={`m-3 ${!password? "text-gray-400":""}`}/>
                 <button className="p-2 border-l-2 border-gray-300">
                     <img className="w-6"  src={copyImg} alt="copy image"/>
                 </button>
